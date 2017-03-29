e6e442be779d8ce96c764a8e46ba6c5387d00e4f
“gitconfig.username” <gitconfig@gmail.com>
2017-03-29 14:51:31 +0800

    update

---
+++ utilslib/src/main/java/com/bewant2be/doit/utilslib/DebugUtil.java
@@ -0,0 +1,20 @@
+package com.bewant2be.doit.utilslib;
+
+/**
+ * Created by user on 3/29/17.
+ */
+public class DebugUtil {
+    public final static String byte2hex(byte [] buffer){
+        String h = "";
+
+        for(int i = 0; i < buffer.length; i++){
+            String temp = Integer.toHexString(buffer[i] & 0xFF);
+            if(temp.length() == 1){
+                temp = "0" + temp;
+            }
+            h = h + " "+ temp;
+        }
+
+        return h;
+    }
+}
