83278c6cb67c1c2ad36943a4d3b3c99cc6f5053e
“gitconfig.username” <gitconfig@gmail.com>
2017-04-24 10:42:25 +0800

    add ToastView

--- utilslib/src/main/java/com/bewant2be/doit/utilslib/ToastUtil.java
+++ utilslib/src/main/java/com/bewant2be/doit/utilslib/ToastUtil.java
@@ -4,6 +4,13 @@ import android.content.Context;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Handler;
 
@@ -35,6 +42,12 @@ public class ToastUtil {
         ToastUtil.getDefault().toast(context, msg);
     }
 
+    public final static void toastView( final Context context, final String msg ){
+        ToastUtil.getDefault().toastview(context, msg);
+    }
+
+
+    // - - - - - - - -- - - -- - - - - - -- - - - -- - - - - - -- - - - --
 
     private void toast( final Context applicationCtx, final String msg ){
         handler.post(new Runnable() {
@@ -45,6 +58,20 @@ public class ToastUtil {
         });
     }
 
+    private void toastview( final Context applicationCtx, final String msg ){
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast toast = Toast.makeText(applicationCtx, msg, Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                LinearLayout toastView = (LinearLayout) toast.getView();
+                ImageView imageCodeProject = new ImageView(applicationCtx);
+                imageCodeProject.setImageResource(R.drawable.tips);
+                toastView.addView(imageCodeProject, 0);
+                toast.show();
+            }
+        });
+    }
 
     private static Toast toast = null;
     private static void showToast(Context context, String text) {
