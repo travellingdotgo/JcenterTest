d45a21f1fbf41fdb35031aefbbe75e3a19527ac0
“bewant2be” <bewant2be@gmail.com>
2017-05-31 08:08:54 +0800

    update

--- utilslib/src/main/java/com/bewant2be/doit/utilslib/DisplayUtil.java
+++ utilslib/src/main/java/com/bewant2be/doit/utilslib/DisplayUtil.java
@@ -2,6 +2,7 @@ package com.bewant2be.doit.utilslib;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.Surface;
 import android.view.WindowManager;
 
@@ -26,8 +27,84 @@ public class DisplayUtil {
         return degree;
     }
 
-    public static int dip2px(Context context, float dpValue) {
+
+
+
+    /**
+     * 获得屏幕宽度（像素）
+     *
+     * @param activity
+     * @return
+     */
+    public static int getDisplayWidth(Activity activity) {
+        DisplayMetrics metric = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        int width = metric.widthPixels;     // 屏幕宽度（像素）
+        return width;
+    }
+
+    /**
+     * 获得屏幕高度（像素）
+     *
+     * @param activity
+     * @return
+     */
+    public static int getDisplayHeight(Activity activity) {
+        DisplayMetrics metric = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        int height = metric.heightPixels;     // 屏幕高度（像素）
+        return height;
+    }
+
+
+    /**
+     * 将px值转换为dip或dp值，保证尺寸大小不变
+     *
+     * @param context 通过上下文获得显示参数中的屏幕密度
+     * @param pxValue 需要转换的px值
+     * @return
+     */
+    public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * scale + 0.5f);
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    /**
+     * 将dip或dp值转换为px值，保证尺寸大小不变
+     *
+     * @param context  通过上下文获得显示参数中的屏幕密度
+     * @param dipValue 需要转换的dp值
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param context 通过上下文获得显示参数中的屏幕密度
+     * @param pxValue 需要转换的px值
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param context 通过上下文获得显示参数中的屏幕密度
+     * @param spValue 需要转换的sp值
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
     }
 }
