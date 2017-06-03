7273c063d7f4ac80fd9b75fa0fd7b46b76ae54ce
“bewant2be” <bewant2be@gmail.com>
2017-06-03 20:08:14 +0800

    add PopupMenu & GridView

---
+++ app/src/main/java/com/bewant2be/doit/jcentertest/GridViewAdapter.java
@@ -0,0 +1,63 @@
+package com.bewant2be.doit.jcentertest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tian on 12/7/15.
+ */
+public class GridViewAdapter extends ArrayAdapter<GridItem> {
+
+    private Context mContext;
+    private int layoutResourceId;
+    private ArrayList<GridItem> mGridData = new ArrayList<GridItem>();
+
+
+    public GridViewAdapter(Context context, int resource, ArrayList<GridItem> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        this.layoutResourceId = resource;
+        this.mGridData = objects;
+    }
+
+    public void setGridData(ArrayList<GridItem> mGridData) {
+        this.mGridData = mGridData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = ((Activity)mContext).getLayoutInflater();
+            convertView = inflater.inflate(layoutResourceId, parent, false);
+            holder = new ViewHolder();
+            holder.textView = (TextView) convertView.findViewById(R.id.txt_item);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.img_item);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        GridItem item = mGridData.get(position);
+        holder.textView.setText(item.getTitle());
+        Picasso.with(mContext).load(item.getImage()).into(holder.imageView);
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView textView;
+        ImageView imageView;
+    }
+}
