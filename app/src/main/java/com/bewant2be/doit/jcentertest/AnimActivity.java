df76c8505e9dd9c07ff933419a6a0113bdae6a83
“bewant2be” <bewant2be@gmail.com>
2017-06-04 17:03:35 +0800

    add Dynamic LayoutPara for a TextView

--- app/src/main/java/com/bewant2be/doit/jcentertest/AnimActivity.java
+++ app/src/main/java/com/bewant2be/doit/jcentertest/AnimActivity.java
@@ -7,32 +7,62 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class AnimActivity extends AppCompatActivity {
-
-    Button scaleBtn	;
     Animation scaleAnimation;
-
     TextView tv;
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anim);
 
+        layout = (LinearLayout)findViewById(R.id.ll_container);
+
         scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scaleanim);
-        scaleBtn = (Button)findViewById(R.id.btn_animation);
         tv =(TextView)findViewById(R.id.tv);
 
-        scaleBtn.setOnClickListener(new View.OnClickListener() {
+        ((Button)findViewById(R.id.btn_animation)).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 tv.startAnimation(scaleAnimation);
             }
         });
 
+        ((Button)findViewById(R.id.btnLayoutPara)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        tv.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                int tick = (x++) % 10;
+                                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50 * tick, 50 * tick);
+                                //params.setMargins(5, 5, 5, 5);
+                                layout.setLayoutParams(params);
+                            }
+                        });
+                    }
+                };
+                timer.schedule(timerTask, 0, 30);
+            }
+        });
+
+
     }
+
+    int x = 0;
 
 }
