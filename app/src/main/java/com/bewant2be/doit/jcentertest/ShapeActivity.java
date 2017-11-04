038845ccc7dc3d58f36f7a02f815553b6fb6af6a
c <c@g.hk>
2017-11-04 14:53:44 +0800

    update

--- app/src/main/java/com/bewant2be/doit/jcentertest/ShapeActivity.java
+++ app/src/main/java/com/bewant2be/doit/jcentertest/ShapeActivity.java
@@ -2,6 +2,7 @@ package com.bewant2be.doit.jcentertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShapeActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class ShapeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape);
+
+        setEmojiToTextView();
+    }
+
+    private void setEmojiToTextView(){
+        TextView tv = (TextView)findViewById(R.id.tvFont);
+
+        int unicodeJoy = 0x1F602;
+        String emojiString = getEmojiStringByUnicode(unicodeJoy);
+        tv.setText(emojiString);
+    }
+
+    private String getEmojiStringByUnicode(int unicode){
+        return new String(Character.toChars(unicode));
     }
 }
