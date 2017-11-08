96eac18c73fb2936c97a78ea83e82c932e3f28d9
c <c@g.hk>
2017-11-08 14:01:29 +0800

    add EditText  addTextChangedListener

--- app/src/main/java/com/bewant2be/doit/jcentertest/LoginActivity.java
+++ app/src/main/java/com/bewant2be/doit/jcentertest/LoginActivity.java
@@ -2,6 +2,10 @@ package com.bewant2be.doit.jcentertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init(){
+        EditText etOne= (EditText) findViewById(R.id.et_phone);
+        etOne.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                Log.i("Ansen","内容改变之前调用:"+s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.i("Ansen","内容改变，可以去告诉服务器:"+s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Log.i("Ansen","内容改变之后调用:"+s);
+            }
+        });
+
     }
 }
